f7aeee7582c4bafdd52d2cd90f546253a191fe18
hederdavid <hedermd6@gmail.com>
2024-08-25 14:02:30 -0300

    feat: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'royal-blue': '#4169E1',
+        'midnight-blue': '#141c3a'
+      },
+
+      animation: {
+        'smooth-up-down': 'smooth-up-down 8s ease-in-out infinite',
+      },
+      keyframes: {
+        'smooth-up-down': {
+          '0%, 100%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(-15px)', /* Ajuste a altura conforme necessário */
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
