fc5ce3df7077235e188ddb68fe1299ce06efd91c
hederdavid <hedermd6@gmail.com>
2024-08-26 15:52:57 -0300

    feat: about-me  and Scrollbar Customization

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { FustechSectionComponent } from "../../components/fustech-section/fustech-section.component";
 import { SocialIconsComponent } from "../../components/social-icons/social-icons.component";
 import { AboutMeComponent } from "../../components/about-me/about-me.component";
+import { ProjectListComponent } from "../../components/project-list/project-list.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FustechSectionComponent, SocialIconsComponent, AboutMeComponent],
+  imports: [HeaderComponent, FustechSectionComponent, SocialIconsComponent, AboutMeComponent, ProjectListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
